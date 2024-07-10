5bda362585ef5584613870982af5ca5ed5f29b0c
104735K <shk773577@gmail.com>
2024-07-11 05:11:45 +0900

    6th commit

--- src/main/java/com/perfect/bowling/controller/HomeController.java
+++ src/main/java/com/perfect/bowling/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping
+    @GetMapping("/bowling/home")
     public String home() {
         return "home";
     }
