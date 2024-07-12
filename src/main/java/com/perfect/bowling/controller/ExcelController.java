7d93e58f134587f1cf15d9fb38c6bbd8bee05619
104735K <shk773577@gmail.com>
2024-07-13 04:38:15 +0900

    10th commit

--- src/main/java/com/perfect/bowling/controller/ExcelController.java
+++ src/main/java/com/perfect/bowling/controller/ExcelController.java
@@ -1,7 +1,11 @@
 package com.perfect.bowling.controller;
 
 import com.perfect.bowling.dto.UserDto;
+import com.perfect.bowling.entity.GameEntity;
+import com.perfect.bowling.entity.ScoreEntity;
 import com.perfect.bowling.entity.UserEntity;
+import com.perfect.bowling.repository.GameRepository;
+import com.perfect.bowling.repository.ScoreRepository;
 import com.perfect.bowling.repository.UserRepository;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
@@ -12,13 +16,17 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/bowling")
@@ -26,6 +34,8 @@ import java.util.List;
 public class ExcelController {
 
     private UserRepository userRepository;
+    private GameRepository gameRepository;
+    private ScoreRepository scoreRepository;
 
     @GetMapping("/downloadUser")
     public void downloadUser(HttpServletResponse response) throws IOException {
@@ -75,6 +85,73 @@ public class ExcelController {
             workbook.close();
             outputStream.close();
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @GetMapping("/downloadScoresByGameId/{gameId}")
+    public void downloadScoresByGameId(HttpServletResponse response, @PathVariable Long gameId, GameEntity game) throws IOException {
+
+        try {
+            Optional<GameEntity> gameEntities = gameRepository.findById(gameId);
+
+            if (gameEntities.isPresent()) {
+                GameEntity gameEntity = gameEntities.get();
+
+                List<ScoreEntity> scoreEntities = scoreRepository.findByGameId(game);
+
+                Workbook workbook = new XSSFWorkbook();
+                Sheet sheet = workbook.createSheet("게임점수");
+                int rowNo = 0;
+
+                Row headerRow = sheet.createRow(rowNo++);
+                headerRow.createCell(0).setCellValue("게임ID");
+                headerRow.createCell(1).setCellValue("게임날짜");
+
+                Row dataRow = sheet.createRow(rowNo++);
+                dataRow.createCell(0).setCellValue(gameEntity.getGameId());
+
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+                String formattedDate = (gameEntity.getGameDate() != null) ? formatter.format(gameEntity.getGameDate()) : "";
+                dataRow.createCell(1).setCellValue(formattedDate);
+
+                rowNo++;
+
+                Row memberHeaderRow = sheet.createRow(rowNo++);
+                memberHeaderRow.createCell(0).setCellValue("회원ID");
+                memberHeaderRow.createCell(1).setCellValue("이름");
+                memberHeaderRow.createCell(2).setCellValue("G1");
+                memberHeaderRow.createCell(3).setCellValue("G2");
+                memberHeaderRow.createCell(4).setCellValue("G3");
+                memberHeaderRow.createCell(5).setCellValue("G4");
+                memberHeaderRow.createCell(6).setCellValue("핸디");
+                memberHeaderRow.createCell(7).setCellValue("총점수");
+                memberHeaderRow.createCell(8).setCellValue("평균");
+
+                for (ScoreEntity scoreEntity : scoreEntities) {
+                    Row MemberDataRow = sheet.createRow(rowNo++);
+                    MemberDataRow.createCell(0).setCellValue(scoreEntity.getUserId().getUserId());
+                    MemberDataRow.createCell(1).setCellValue(scoreEntity.getUserName());
+                    MemberDataRow.createCell(2).setCellValue(scoreEntity.getScoreG1());
+                    MemberDataRow.createCell(3).setCellValue(scoreEntity.getScoreG2());
+                    MemberDataRow.createCell(4).setCellValue(scoreEntity.getScoreG3());
+                    MemberDataRow.createCell(6).setCellValue(scoreEntity.getScoreG4());
+                    MemberDataRow.createCell(6).setCellValue(scoreEntity.getHandicap());
+                    MemberDataRow.createCell(7).setCellValue(scoreEntity.getTotalScore());
+                    MemberDataRow.createCell(8).setCellValue(scoreEntity.getAvgScore());
+                }
+
+                response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+                response.setHeader("Content-Disposition", "attachment; filename=users.xlsx");
+
+                OutputStream outputStream = response.getOutputStream();
+                workbook.write(outputStream);
+                workbook.close();
+                outputStream.close();
+            } else {
+                response.getWriter().println("해당 게임ID에 대한 데이터가 존재하지 않습니다.");
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/main/java/com/perfect/bowling/repository/ScoreRepository.java
+++ src/main/java/com/perfect/bowling/repository/ScoreRepository.java
@@ -1,8 +1,11 @@
 package com.perfect.bowling.repository;
 
+import com.perfect.bowling.entity.GameEntity;
 import com.perfect.bowling.entity.ScoreEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ScoreRepository extends JpaRepository<ScoreEntity, Long> { }
+public interface ScoreRepository extends JpaRepository<ScoreEntity, Long> {
+    List<ScoreEntity> findByGameId(GameEntity gameId);
+}
